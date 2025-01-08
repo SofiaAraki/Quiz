78fa74e14c7f6d908b4ece865a7aa3ae68432d71
SofiaAraki <fernandasofia589@gmail.com>
2025-01-08 15:41:27 -0300

    first commit

---
+++ api.js
@@ -0,0 +1,16 @@
+const axios = require('axios')
+
+
+apiKey='8ejhebUUUT8Wy2peVVWK3KKdiUfMUxofo9KqeTLp'
+category='code'
+difficulty='easy'
+tags='python'
+limit=10
+
+url=`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${limit}&category=${category}&difficulty=${difficulty}&tags=${tags}`
+
+const getQuestions = async () => {
+    return axios.get(url);
+}
+
+console.log(getQuestions());
