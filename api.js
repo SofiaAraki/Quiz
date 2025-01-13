68f78655e0ae4a219f9f73dbd9f456f7a8c965c9
SofiaAraki <fernandasofia589@gmail.com>
2025-01-13 00:30:35 -0300

    ui complete

--- api.js
+++ api.js
@@ -3,11 +3,11 @@ const axios = require('axios')
 
 apiKey='8ejhebUUUT8Wy2peVVWK3KKdiUfMUxofo9KqeTLp'
 category='code'
-difficulty='easy'
-tags='python'
+difficulty='Easy'
+tags='JavaScript'
 limit=10
 
-url=`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&limit=${limit}&category=${category}&difficulty=${difficulty}&tags=${tags}`
+url=`https://quizapi.io/api/v1/questions?apiKey=${apiKey}&category=${category}&difficulty=${difficulty}&limit=${limit}&tags=${tags}`
 
 const getQuestions = async () => {
     return axios.get(url);
